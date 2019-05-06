5f445f16e0d208d1fa246f02a3a53b75ba0ac88c
BJ Hansen <bjhansen@bjhansen-desktop>
2019-05-05 22:29:17 -0500

    Service provider array fixes

--- src/Providers/DiscussionForumProvider.php
+++ src/Providers/DiscussionForumProvider.php
@@ -27,9 +27,12 @@ class DiscussionForumProvider extends ServiceProvider
         // load the module into the master config array
         $enabled_modules = is_array(config('modules.enabled')) ? config('modules.enabled') : [];
         $this_module = [
-            'name' => 'Discussion Forum',
-            'version' => '1.0.0',
-            'class' => self::class
+            [
+                'name' => 'Discussion Forum',
+                'slug' => 'discussion_forum',
+                'version' => '1.0.0',
+                'class' => self::class
+            ]
         ];
         $module_config = array_merge($enabled_modules, $this_module);
         config([
